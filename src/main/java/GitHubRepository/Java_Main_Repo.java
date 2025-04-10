953036ddde1b27f231fd4e2410d2fd43d6a60bf7
Priyank Patel <you@example.com>
2025-04-10 21:06:45 +0530

    initail commit

---
+++ src/main/java/GitHubRepository/Java_Main_Repo.java
@@ -0,0 +1,10 @@
+package GitHubRepository;
+
+public class Java_Main_Repo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/GitHubRepository/Java_Test_Repo.java
@@ -0,0 +1,10 @@
+package GitHubRepository;
+
+public class Java_Test_Repo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
